9b8e8a4fbe968dd902519c28998173e837d105d4
bagir4ik <109833905+bagir4ik@users.noreply.github.com>
2022-08-02 15:37:11 +0300

    Update THE ROCK.py

--- THE ROCK.py
+++ THE ROCK.py
@@ -8,7 +8,7 @@ for l1 in alf:
                     for l6 in alf:
                         word = l1 + l2 + l3 + l4 + l5 + l6      
                         if word[0] != 'Ь' and \
-                        not('АЕ' in word or 'ЕА'in word) \
+                        not('АЕ' in word or 'ЕА' in word) \
                         and word.count('К') == 0:
                             count += 1
 print(count)
